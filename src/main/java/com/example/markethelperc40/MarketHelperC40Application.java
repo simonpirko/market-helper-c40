bf652464747dcfc9ae090d90872071690d385d55
Simon Pirko <simonpirko@ridelex.io>
2021-06-30 19:21:06 +0300

    Initial commit

---
+++ src/main/java/com/example/markethelperc40/MarketHelperC40Application.java
@@ -0,0 +1,13 @@
+package com.example.markethelperc40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketHelperC40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketHelperC40Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/markethelperc40/MarketHelperC40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markethelperc40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketHelperC40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
